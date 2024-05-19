6396109b3d28dfbe62ac1b3309b0d2c49a549ff8
Harry Lewandowski <harrylewandowski04@gmail.com>
2024-05-19 19:30:26 +1000

    Created project: Data storage, data variables, work started on note content UI

---
+++ snowNotes/noteView.swift
@@ -0,0 +1,18 @@
+//
+//  noteView.swift
+//  snowNotes
+//
+//  Created by Harry Lewandowski on 19/5/2024.
+//
+
+import SwiftUI
+
+struct noteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    noteView()
+}
